50c83b836e834090adb833cc327af50043e9f1a5
r0tiart <antley.chu@gmail.com>
2018-03-05 18:17:54 -0500

    question 2 permutation

--- js/datastructures.js
+++ js/datastructures.js
@@ -1,5 +1,5 @@
 /* Is Unique? Implement an algorithm to determine if a string has all unique characters. 
-What if you cannot use additional data structures?    */                                                                                                                                      
+What if you cannot use additional data structures?   Hints : #44, #117, #132 */                                                                                                                                      
 
 var unique = function(string) {
   var strArr = string.split(""),
@@ -15,4 +15,40 @@ var unique = function(string) {
   
   return true
   
+}
+
+/*
+  Permutation - Given two strings, write a method to decide if one is a permutation of the other.
+  Hints: #1, #84, #122, #131
+*/
+
+var permutation = function(str1, str2){
+  
+  var str1Arr = str1.split(""),
+      str2Arr = str2.split(""),
+      str1map = {};
+  
+  
+  for(let i = 0; i < str1.length; i++){
+    if(str1map[str1Arr[i]]){
+      
+      str1map[str1Arr[i]] += 1; 
+    } else {
+      
+      str1map[str1Arr[i]] = 1;
+    }
+  }
+  
+  
+  for( let j = 0; j < str2.length; j++ ){
+    if(str1map[str2Arr[j]]){
+      
+      str1map[str2Arr[j]] > 0 ? str1map[str2Arr[j]] -= 1: delete(str1map[str2Arr[j]])
+      
+    } else {
+      return false
+    }
+  }
+  
+  return true
 }
